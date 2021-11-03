f52a4d74b49a6e93237dbc19aabc8cb2e4294482
renanarimon <77155986+renanarimon@users.noreply.github.com>
2021-11-03 14:08:49 +0200

    initial commit

---
+++ src/main.java
@@ -0,0 +1,36 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+
+public class main {
+
+    public static void main(String[] args) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse("alarm_net.xml");
+            NodeList nameList = doc.getElementsByTagName("NAME");
+            for (int i = 0; i < nameList.getLength(); i++) {
+                Node n = nameList.item(i);
+                if (n.getNodeType() == Node.ELEMENT_NODE){
+                    Element name = (Element) n;
+                    String outcome =  name.getAttribute("OUTCOME");
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+    }
+}
