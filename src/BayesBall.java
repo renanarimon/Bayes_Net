af23d1251f5f576b76321d76927042f6860777bd
renanarimon <77155986+renanarimon@users.noreply.github.com>
2021-11-15 02:03:13 +0200

    update bayesBall

--- src/BayesBall.java
+++ src/BayesBall.java
@@ -20,19 +20,19 @@ public class BayesBall {
 
 
     public void goOverQ() {
-        for (int j=0; j<net.getQueryBayesBall().size(); j++){
-            for (NetNode n:net.getBayesNet().values()) {
+        for (int j = 0; j < net.getQueryBayesBall().size(); j++) {
+            for (NetNode n : net.getBayesNet().values()) {
                 n.setGiven(null);
                 n.fromChild = false;
                 n.fromParent = false;
             }
             String q = net.getQueryBayesBall().get(j);
-            NetNode start = net.getBayesNet().get(q.charAt(0)+"");
-            NetNode end = net.getBayesNet().get(q.charAt(2)+"");
-            for (int i=4; i<q.length(); i+=4){
-                net.getBayesNet().get(q.charAt(i)+"").setGiven(q.charAt(i+2)+"");
+            NetNode start = net.getBayesNet().get(q.charAt(0) + "");
+            NetNode end = net.getBayesNet().get(q.charAt(2) + "");
+            for (int i = 4; i < q.length(); i += 4) {
+                net.getBayesNet().get(q.charAt(i) + "").setGiven(q.charAt(i + 2) + "");
             }
-            dfs(start,end);
+            dfs(start, end);
         }
 
     }
@@ -40,44 +40,50 @@ public class BayesBall {
     public void dfs(NetNode start, NetNode end) {
         Stack<NetNode> stack = new Stack<NetNode>();
         stack.push(start);
+        NetNode curr = stack.pop();
+        pushChildren(curr);
+        pushParents(curr);
+
         while (!stack.isEmpty()) {
-            if (stack.contains(end)){ //there is a path --> depended
+            if (stack.contains(end)) { //there is a path --> depended
                 System.out.println("no");
                 return;
             }
-            NetNode curr = stack.pop();
+            curr = stack.pop();
 
-            if (curr.getGiven() == null) { //not colored
-                for (String c : curr.Children) { //push children to stack
-                    NetNode child = net.getBayesNet().get(c);
-                    if (!child.fromParent) {
-                        child.fromParent = true;
-                        stack.push(child);
-                    }
-                }
+            if (curr.getGiven() == null) { //not given
+                pushChildren(curr);
                 if (curr.fromChild) {
-                    for (String p : curr.Parents) {
-                        NetNode parent = net.getBayesNet().get(p);
-                        if (!parent.fromChild) {
-                            parent.fromChild = true;
-                            stack.push(parent);
-                        }
-                    }
+                    pushParents(curr);
                 }
-            } else { //colored
+            } else { //given
                 if (curr.fromParent) {
-                    for (String p : curr.Parents) {
-                        NetNode parent = net.getBayesNet().get(p);
-                        if (!parent.fromChild) {
-                            parent.fromChild = true;
-                            stack.push(parent);
-                        }
-                    }
+                    pushParents(curr);
                 }
             }
 
         }
         System.out.println("yes"); // no path --> not depended
+    }
+
+    private void pushParents(NetNode curr) {
+        for (String p : curr.Parents) {
+            NetNode parent = net.getBayesNet().get(p);
+            if (!parent.fromChild) {
+                parent.fromChild = true;
+                stack.push(parent);
+            }
+        }
+    }
+
+    private void pushChildren(NetNode curr) {
+        for (String c : curr.Children) { //push children to stack
+            NetNode child = net.getBayesNet().get(c);
+            if (!child.fromParent) {
+                child.fromParent = true;
+                stack.push(child);
+            }
+        }
     }
 
 
